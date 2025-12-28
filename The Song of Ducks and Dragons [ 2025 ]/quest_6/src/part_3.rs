75a6473c503a83e880f63a75da6383d8b7569384
BlankWasThere <186826343+BlankWasThere@users.noreply.github.com>
2025-12-29 01:09:07 +0530

    feat: add solution to 2025/quest_6 (part 3)

--- The Song of Ducks and Dragons [ 2025 ]/quest_6/src/part_3.rs
+++ The Song of Ducks and Dragons [ 2025 ]/quest_6/src/part_3.rs
@@ -1,3 +1,41 @@
-pub fn solve(_input: &str) -> anyhow::Result<()> {
-    todo!()
+pub fn solve(input: &str) -> anyhow::Result<()> {
+    const MAX_DISTANCE: usize = 1000;
+    const REPETITIONS: usize = 1000;
+
+    let chars = input.trim().chars().collect::<Vec<_>>();
+
+    let one = calculate(&chars, 1, MAX_DISTANCE);
+    let two = calculate(&chars, 2, MAX_DISTANCE);
+    let diff = two - one;
+
+    let count = one + diff * (REPETITIONS - 1) as u64;
+
+    println!("Answer: {count}");
+    Ok(())
+}
+
+fn calculate(chars: &[char], repetitions: usize, max_distance: usize) -> u64 {
+    let mut count = 0_u64;
+
+    for (i, &c) in chars
+        .iter()
+        .cycle()
+        .take(chars.len() * repetitions)
+        .enumerate()
+    {
+        if c.is_ascii_lowercase() {
+            let li = i.saturating_sub(max_distance);
+            let ri = (i + max_distance).min(chars.len() * repetitions - 1);
+
+            count += chars
+                .iter()
+                .cycle()
+                .skip(li)
+                .take(ri - li + 1)
+                .filter(|&&m| m.is_ascii_uppercase() && m.to_ascii_lowercase() == c)
+                .count() as u64;
+        }
+    }
+
+    count
 }
