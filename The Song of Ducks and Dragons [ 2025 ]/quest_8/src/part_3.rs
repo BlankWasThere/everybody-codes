3b1ecd3ad9afcd7f366375681fbf770907d8b1f9
BlankWasThere <186826343+BlankWasThere@users.noreply.github.com>
2025-12-31 00:56:11 +0530

    feat: add solution to 2025/quest_8 (part 3)

--- The Song of Ducks and Dragons [ 2025 ]/quest_8/src/part_3.rs
+++ The Song of Ducks and Dragons [ 2025 ]/quest_8/src/part_3.rs
@@ -1,3 +1,39 @@
-pub fn solve(_input: &str) -> anyhow::Result<()> {
-    todo!();
+fn parse_input(input: &str) -> anyhow::Result<Vec<u32>> {
+    input
+        .trim()
+        .split(',')
+        .filter_map(|s| {
+            let s = s.trim();
+            if !s.is_empty() { Some(s) } else { None }
+        })
+        .map(|s| Ok(s.parse()?))
+        .collect::<anyhow::Result<Vec<_>>>()
+}
+
+pub fn solve(input: &str) -> anyhow::Result<()> {
+    const TOTAL: u32 = 256;
+
+    let numbers = parse_input(input)?;
+    let mut max_cuts = 0;
+
+    for prev in 1..=TOTAL {
+        for next in 1..=TOTAL {
+            let mut count = 0;
+
+            for (&chk_prev, &chk_next) in numbers.iter().zip(numbers.iter().skip(1)) {
+                if ((prev < chk_prev && chk_prev < next) && (chk_next < prev || chk_next > next))
+                    || ((prev < chk_next && chk_next < next)
+                        && (chk_prev < prev || chk_prev > next))
+                {
+                    count += 1;
+                }
+            }
+
+            max_cuts = max_cuts.max(count)
+        }
+    }
+
+    println!("Answer: {max_cuts}");
+
+    Ok(())
 }
