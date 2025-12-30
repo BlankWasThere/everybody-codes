2b1ab5e2fb478b10dc3cd110b4d6106907ff0c6e
BlankWasThere <186826343+BlankWasThere@users.noreply.github.com>
2025-12-31 00:29:55 +0530

    feat: add solution to 2025/quest_8 (part 2)

--- The Song of Ducks and Dragons [ 2025 ]/quest_8/src/part_2.rs
+++ The Song of Ducks and Dragons [ 2025 ]/quest_8/src/part_2.rs
@@ -1,3 +1,40 @@
-pub fn solve(_input: &str) -> anyhow::Result<()> {
-    todo!();
+macro_rules! simple_min_max {
+    ($x:expr, $y:expr) => {{
+        let x = $x;
+        let y = $y;
+        if x > y { (y, x) } else { (x, y) }
+    }};
+}
+
+fn parse_input(input: &str) -> anyhow::Result<Vec<u32>> {
+    input
+        .trim()
+        .split(',')
+        .filter_map(|s| {
+            let s = s.trim();
+            if !s.is_empty() { Some(s) } else { None }
+        })
+        .map(|s| Ok(s.parse()?))
+        .collect::<anyhow::Result<Vec<_>>>()
+}
+
+pub fn solve(input: &str) -> anyhow::Result<()> {
+    let numbers = parse_input(input)?;
+    let mut count = 0;
+
+    for (idx, (&prev, &next)) in numbers.iter().zip(numbers.iter().skip(1)).enumerate() {
+        let (prev, next) = simple_min_max!(prev, next);
+
+        for (&chk_prev, &chk_next) in numbers.iter().zip(numbers.iter().skip(1)).take(idx) {
+            if ((prev < chk_prev && chk_prev < next) && (chk_next < prev || chk_next > next))
+                || ((prev < chk_next && chk_next < next) && (chk_prev < prev || chk_prev > next))
+            {
+                count += 1;
+            }
+        }
+    }
+
+    println!("Answer: {count}");
+
+    Ok(())
 }
