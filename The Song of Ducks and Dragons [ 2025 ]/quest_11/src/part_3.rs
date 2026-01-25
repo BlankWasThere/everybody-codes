6d248150cb6979f25dfca703df683a68b768b9ca
BlankWasThere <186826343+BlankWasThere@users.noreply.github.com>
2026-01-26 00:03:46 +0530

    feat: add solution to 2025/quest_11 (part 3)

--- The Song of Ducks and Dragons [ 2025 ]/quest_11/src/part_3.rs
+++ The Song of Ducks and Dragons [ 2025 ]/quest_11/src/part_3.rs
@@ -1,3 +1,24 @@
+fn parse_input(input: &str) -> anyhow::Result<Vec<u64>> {
+    input
+        .lines()
+        .map(str::trim)
+        .filter(|s| !s.is_empty())
+        .map(|s| Ok(s.parse()?))
+        .collect()
+}
+
 pub fn solve(input: &str) -> anyhow::Result<()> {
-    todo!()
+    let columns = parse_input(input)?;
+
+    // This code assumes that phase 1 is skipped
+    let mean = columns.iter().sum::<u64>() / columns.len() as u64;
+    let rounds = columns
+        .into_iter()
+        .filter(|&n| n < mean)
+        .map(|n| mean - n)
+        .sum::<u64>();
+
+    println!("Answer: {rounds}");
+
+    Ok(())
 }
