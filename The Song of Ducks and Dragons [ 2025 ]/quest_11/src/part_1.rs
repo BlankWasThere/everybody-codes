dc26d514fb28a58da4b99d2c561dec9e9b1cb323
BlankWasThere <186826343+BlankWasThere@users.noreply.github.com>
2026-01-25 22:47:46 +0530

    feat: add solution to 2025/quest_11 (part 1)

--- The Song of Ducks and Dragons [ 2025 ]/quest_11/src/part_1.rs
+++ The Song of Ducks and Dragons [ 2025 ]/quest_11/src/part_1.rs
@@ -1,3 +1,75 @@
+fn parse_input(input: &str) -> anyhow::Result<Vec<u32>> {
+    input
+        .lines()
+        .map(str::trim)
+        .filter(|s| !s.is_empty())
+        .map(|s| Ok(s.parse()?))
+        .collect()
+}
+
 pub fn solve(input: &str) -> anyhow::Result<()> {
-    todo!()
+    let mut columns = parse_input(input)?;
+
+    let mut checksum = 0;
+    let mut rounds = 0;
+    let mut phase = 0;
+
+    while rounds < 10 {
+        rounds += 1;
+
+        match phase {
+            // First phase
+            0 => {
+                checksum = 0;
+                let mut moved = false;
+
+                for (i, j) in (0..columns.len()).zip(1..columns.len()) {
+                    if columns[i] > columns[j] {
+                        columns[j] += 1;
+                        columns[i] -= 1;
+
+                        moved = true;
+                    }
+
+                    checksum += (i + 1) as u32 * columns[i];
+                }
+
+                checksum += columns.len() as u32 * columns.last().unwrap_or(&0);
+
+                // No more movement = next phase
+                if !moved {
+                    rounds -= 1;
+                    phase = 1;
+                }
+            }
+            // Second phase
+            1 => {
+                checksum = 0;
+                let mut moved = false;
+
+                for (i, j) in (0..columns.len()).zip(1..columns.len()) {
+                    if columns[j] > columns[i] {
+                        columns[i] += 1;
+                        columns[j] -= 1;
+
+                        moved = true;
+                    }
+
+                    checksum += (i + 1) as u32 * columns[i];
+                }
+
+                checksum += columns.len() as u32 * columns.last().unwrap_or(&0);
+
+                // No more movement = completed
+                if !moved {
+                    break;
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    println!("Answer: {checksum}");
+
+    Ok(())
 }
