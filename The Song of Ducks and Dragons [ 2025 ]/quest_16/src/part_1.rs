256c62a1b6aa95424bc448d055f122704e1907e1
BlankWasThere <186826343+BlankWasThere@users.noreply.github.com>
2026-03-14 13:17:54 +0530

    feat: add solution to 2025/quest_16 (part 1)

--- The Song of Ducks and Dragons [ 2025 ]/quest_16/src/part_1.rs
+++ The Song of Ducks and Dragons [ 2025 ]/quest_16/src/part_1.rs
@@ -1,3 +1,23 @@
+fn parse_input(input: &str) -> anyhow::Result<Vec<u32>> {
+    input.trim().split(',').map(|n| Ok(n.parse()?)).collect()
+}
+
 pub fn solve(input: &str) -> anyhow::Result<u32> {
-    todo!()
+    const LENGTH: u32 = 90;
+    let pattern = parse_input(input)?;
+
+    Ok(pattern.into_iter().map(|n| LENGTH / n).sum())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let actual = solve("1,2,3,5,9").unwrap();
+        let expected = 193;
+
+        assert_eq!(expected, actual);
+    }
 }
